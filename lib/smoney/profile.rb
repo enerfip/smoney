8627c938c57d5685d9327f88753dec523d251235
arnaud sellenet <arnodmental@gmail.com>
2019-10-29 11:08:50 +0100

    Change profile field gender to civility

--- lib/smoney/profile.rb
+++ lib/smoney/profile.rb
@@ -6,17 +6,15 @@ module Smoney
 
     collection
 
-    enum      :gender, options: { male: 0, female: 1 }
+    enum      :civility, options: { male: 0, female: 1 }
     value     :first_name
     value     :last_name
     date      :birthdate
+    value     :birthcity, map_to: "Birthcity"
+    value     :birthcountry, map_to: "BirthCountry"
     value     :phone, map_to: "PhoneNumber"
     value     :email
     value     :nickname, map_to: "Alias"
     object    :address
-
-    def civility
-      male? ? "Monsieur" : "Madame"
-    end
   end
 end
